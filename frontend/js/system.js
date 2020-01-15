c2863e3f899aa90bbc62dc285ef4333b290f9870
Richard Neumann <r.neumann@homeinfo.de>
2020-01-15 11:47:40 +0100

    Fix.

--- frontend/js/system.js
+++ frontend/js/system.js
@@ -25,47 +25,17 @@ var sysmon = sysmon || {};
 
 
 sysmon.renderDiagram = function (records) {
-    const points = [];
+    const data = [];
 
     for (const record of records) {
-        points.push([record.timestamp, record.online ? 1 : -1]);
+        const item = {x: new Date(record.timestamp), y: record.online ? 1 : -1};
+        data.push(item);
     }
 
     const context = document.getElementById('uptime').getContext('2d');
     const chart = new Chart(context, {
         type: 'line',
-        data: {
-            datasets: [{
-                label: '# of Votes',
-                data: points,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
+        data: data
     });
     chart.render();
     sysmon.stopLoading();
