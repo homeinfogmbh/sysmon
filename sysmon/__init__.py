2aaa52bccfb678c4eeea858bff085e33363edba7
Richard Neumann <r.neumann@homeinfo.de>
2020-01-23 10:22:16 +0100

    Fix.

--- sysmon/__init__.py
+++ sysmon/__init__.py
@@ -1,13 +1,13 @@
 """HOMEINFO digital signage systems monitoring."""
 
-from sysmon.daemon import run_checks
+from sysmon.daemon import spawn
 from sysmon.orm import SystemCheck, OnlineCheck, ApplicationCheck
 from sysmon.wsgi import APPLICATION
 
 
 __all__ = [
     'APPLICATION',
-    'run_checks',
+    'spawn',
     'SystemCheck',
     'OnlineCheck',
     'ApplicationCheck'
