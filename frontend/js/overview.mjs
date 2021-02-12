0bcc808144856e354866afa63cd34288e58b92dd
Richard Neumann <mail@richard-neumann.de>
2021-02-12 16:14:13 +0100

    Updated copyright year.

--- frontend/js/overview.mjs
+++ frontend/js/overview.mjs
@@ -1,7 +1,7 @@
 /*
     overview.js - Systems monitoring overview.
 
-    (C) 2019 HOMEINFO - Digitale Informationssysteme GmbH
+    (C) 2019-2021 HOMEINFO - Digitale Informationssysteme GmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
