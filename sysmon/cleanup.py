693873b9b799f7cb1287a711458c8eef68fdf4ab
Richard Neumann <r.neumann@homeinfo.de>
2022-04-13 12:51:04 +0200

    Rewrite cleanup script

--- sysmon/cleanup.py
+++ sysmon/cleanup.py
@@ -3,39 +3,24 @@
 from argparse import ArgumentParser, Namespace
 from datetime import datetime, timedelta
 from logging import INFO, basicConfig, getLogger
-from typing import Iterable, Iterator
-
-from peewee import ModelBase
 
 from sysmon.config import LOG_FORMAT
-from sysmon.orm import CHECKS
+from sysmon.orm import CheckResults
 
 
 __all__ = ['main']
 
 
 LOGGER = getLogger('sysmon-cleanup')
-TABLES = {table.__name__: table for table in CHECKS}
-
-
-def get_tables(names: Iterable[str]) -> Iterator[ModelBase]:
-    """Returns the respective type by its name."""
-
-    for name in names:
-        try:
-            yield TABLES[name]
-        except KeyError:
-            LOGGER.error('No such table: %s', name)
 
 
 def get_args() -> Namespace:
     """Parses the CLI arguments."""
 
     parser = ArgumentParser(description='cleans old records')
-    parser.add_argument('-t', '--tables', nargs='*', default=TABLES.keys(),
-                        help='tables to clean')
-    parser.add_argument('-d', '--days', type=int, default=30,
-                        help='days of records to keep')
+    parser.add_argument(
+        '-d', '--days', type=int, default=30, help='days of records to keep'
+    )
     return parser.parse_args()
 
 
@@ -44,10 +29,8 @@ def main() -> None:
 
     args = get_args()
     basicConfig(format=LOG_FORMAT, level=INFO)
-    timestamp = datetime.now() - timedelta(days=args.days)
     LOGGER.info('Deleting records older than %i days.', args.days)
-
-    for table in get_tables(args.tables):
-        LOGGER.info('Cleaning up table: %s', table.__name__)
-        count = table.delete().where(table.timestamp < timestamp).execute()
-        LOGGER.info('Deleted %i records.', count)
+    count = CheckResults.delete().where(
+        CheckResults.timestamp < datetime.now() - timedelta(days=args.days)
+    ).execute()
+    LOGGER.info('Deleted %i records.', count)
