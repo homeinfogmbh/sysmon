32f8e3e5fa6354dafc2ba843591cac65b328a587
Richard Neumann <r.neumann@homeinfo.de>
2022-04-20 13:46:13 +0200

    Move stats into sub-object

--- sysmon/json.py
+++ sysmon/json.py
@@ -9,6 +9,7 @@ from hwdb import Deployment, System
 from sysmon.filters import application_not_running
 from sysmon.filters import more_than_three_months_offline
 from sysmon.filters import not_deployed
+from sysmon.filters import os_out_of_date
 from sysmon.filters import smart_check_failed
 from sysmon.filters import testing
 from sysmon.orm import CheckResults
@@ -21,21 +22,51 @@ def to_dict(check_results: Iterable[CheckResults]) -> dict[str, Any]:
     """Creates the response dict."""
 
     return {
-        'log': [check_result.to_json() for check_result in check_results],
-        'smartFailures': count(filter(
-            smart_check_failed,
-            (last := last_check_of_each_system(
-                system_checks := check_results_by_systems(check_results)
-            ))
+        'log': dict(serialize_by_system(
+            system_checks := check_results_by_systems(check_results)
         )),
-        'notDeployed': count(filter(not_deployed, system_checks)),
-        'testing': count(filter(testing, unique_deployments(system_checks))),
-        'applicationNotRunning': count(filter(application_not_running, last)),
-        'oldOs': 0,     # TODO: implement
-        'moreThanThreeMonthsOffline': count(filter(
-            more_than_three_months_offline, last
-        ))
+        'stats': {
+            'smartFailures': count(filter(
+                smart_check_failed,
+                (last := last_check_of_each_system(system_checks))
+            )),
+            'notDeployed': count(filter(not_deployed, system_checks)),
+            'testing': count(filter(
+                testing, unique_deployments(system_checks)
+            )),
+            'applicationNotRunning': count(filter(
+                application_not_running, last
+            )),
+            'osOutOfDate': count(filter(os_out_of_date, last)),
+            'moreThanThreeMonthsOffline': count(filter(
+                more_than_three_months_offline, last
+            ))
+        }
     }
+
+
+def serialize_by_system(
+        system_checks: dict[System, list[CheckResults]]
+) -> Iterator[tuple[int, dict[str, Any]]]:
+    """Serialize check results grouped by system."""
+
+    for system, check_results in system_checks.items():
+        yield system.id, serialize_system_and_checks(system, check_results)
+
+
+def serialize_system_and_checks(
+        system: System,
+        check_results: Iterable[CheckResults]
+) -> dict[str, Any]:
+    """Serialize a systems and its check results."""
+
+    json = system.to_json()
+    json['checkResults'] = checks_results_json = []
+
+    for check_result in check_results:
+        checks_results_json.append(check_result.to_json())
+
+    return json
 
 
 def count(items: Iterable[Any]) -> int:
