fd6e09d7802984af00bf8aec818a8da06ceba085
Richard Neumann <r.neumann@homeinfo.de>
2022-04-21 16:53:40 +0200

    Use cascaded select

--- sysmon/daemon.py
+++ sysmon/daemon.py
@@ -2,9 +2,7 @@
 
 from logging import INFO, basicConfig
 
-from peewee import JOIN
-
-from hwdb import Deployment, System
+from hwdb import System
 
 from sysmon.checks import check_systems
 from sysmon.config import LOG_FORMAT
@@ -17,9 +15,4 @@ def spawn() -> None:
     """Runs the daemon."""
 
     basicConfig(level=INFO, format=LOG_FORMAT)
-    check_systems(
-        System.select(System, Deployment).join(
-            Deployment, on=System.deployment == Deployment.id,
-            join_type=JOIN.LEFT_OUTER
-        )
-    )
+    check_systems(System.select(cascade=True))
