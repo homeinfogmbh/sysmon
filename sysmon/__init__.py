bec3a5dc5539d329969d990a0c24c67e3139cf19
Richard Neumann <r.neumann@homeinfo.de>
2022-04-21 14:36:14 +0200

    Update exports

--- sysmon/__init__.py
+++ sysmon/__init__.py
@@ -1,14 +1,10 @@
 """HOMEINFO digital signage systems monitoring."""
 
-from sysmon.notify import notify
-from sysmon.orm import SystemCheck, OnlineCheck, ApplicationCheck
+from sysmon.orm import CheckResults
 from sysmon.wsgi import APPLICATION
 
 
 __all__ = [
     'APPLICATION',
-    'notify',
-    'SystemCheck',
-    'OnlineCheck',
-    'ApplicationCheck'
+    'CheckResults'
 ]
