68232c7c01bed9708c35698c0c71b4196175c6d9
Richard Neumann <r.neumann@homeinfo.de>
2023-03-22 09:53:28 +0100

    Outsource MonthlyStats into own module

--- sysmon/mailing.py
+++ sysmon/mailing.py
@@ -3,14 +3,13 @@
 from __future__ import annotations
 from collections import defaultdict
 from datetime import date, timedelta
-from math import floor
-from typing import Iterable, Iterator, NamedTuple
+from typing import Iterable, Iterator
 
 from emaillib import EMail
 from hwdb import Deployment, System
 from mdb import Customer
 
-from sysmon.enumerations import SuccessFailedUnsupported
+from sysmon.monthly_stats import MonthlyStats
 from sysmon.orm import CheckResults
 
 
@@ -35,75 +34,6 @@ technik@homeinfo-medienservice.de
 '''
 SENDER = 'info@homeinfo.de'
 SUBJECT = 'HOMEINFO: Displaystatistik {date}'
-MIN_DOWNLOAD = 1953.125     # Kilobits/s
-MIN_UPLOAD = 488.28125      # Kilobits/s
-
-
-class MonthlyStats(NamedTuple):
-    """Monthly display stats."""
-
-    systems: frozenset[System]
-    online: frozenset[System]
-    download_critical: frozenset[System]
-    upload_critical: frozenset[System]
-    not_fitted: frozenset[System]
-    overheated: frozenset[System]
-
-    @property
-    def percent_online(self) -> int:
-        """Return percentage of online systems."""
-        return floor(len(self.online) / len(self.systems) * 100)
-
-    @property
-    def upload_download_critical(self) -> frozenset[System]:
-        """Return the systems where upload and/or download are critical."""
-        return self.download_critical | self.upload_critical
-
-    @classmethod
-    def from_system_check_results(
-            cls,
-            system_check_results: dict[System, Iterable[CheckResults]]
-    ) -> MonthlyStats:
-        """Create monthly stats from monthly check results."""
-        online = set()
-        download_critical = set()
-        upload_critical = set()
-        not_fitted = set()
-        overheated = set()
-
-        for system, check_results in system_check_results.items():
-            if any(check_result.online for check_result in check_results):
-                online.add(system)
-
-            if all(
-                    check_result.download < MIN_DOWNLOAD
-                    for check_result in check_results
-            ):
-                download_critical.add(system)
-
-            if all(
-                    check_result.upload < MIN_UPLOAD
-                    for check_result in check_results
-            ):
-                upload_critical.add(system)
-
-            if system.deployment is not None and not system.fitted:
-                not_fitted.add(system)
-
-            if all(
-                    check_result.sensors is SuccessFailedUnsupported.FAILED
-                    for check_result in check_results
-            ):
-                overheated.add(system)
-
-        return MonthlyStats(
-            frozenset(system_check_results),
-            frozenset(online),
-            frozenset(download_critical),
-            frozenset(upload_critical),
-            frozenset(not_fitted),
-            frozenset(overheated)
-        )
 
 
 def create_emails(
---
+++ sysmon/monthly_stats.py
@@ -0,0 +1,84 @@
+"""Monthly stats."""
+
+from __future__ import annotations
+from math import floor
+from typing import Iterable, NamedTuple
+
+from hwdb import System
+
+from sysmon.enumerations import SuccessFailedUnsupported
+from sysmon.orm import CheckResults
+
+
+__all__ = ['MonthlyStats']
+
+
+MIN_DOWNLOAD = 1953.125     # Kilobits/s
+MIN_UPLOAD = 488.28125      # Kilobits/s
+
+
+class MonthlyStats(NamedTuple):
+    """Monthly display stats."""
+
+    systems: frozenset[System]
+    online: frozenset[System]
+    download_critical: frozenset[System]
+    upload_critical: frozenset[System]
+    not_fitted: frozenset[System]
+    overheated: frozenset[System]
+
+    @property
+    def percent_online(self) -> int:
+        """Return percentage of online systems."""
+        return floor(len(self.online) / len(self.systems) * 100)
+
+    @property
+    def upload_download_critical(self) -> frozenset[System]:
+        """Return the systems where upload and/or download are critical."""
+        return self.download_critical | self.upload_critical
+
+    @classmethod
+    def from_system_check_results(
+            cls,
+            system_check_results: dict[System, Iterable[CheckResults]]
+    ) -> MonthlyStats:
+        """Create monthly stats from monthly check results."""
+        online = set()
+        download_critical = set()
+        upload_critical = set()
+        not_fitted = set()
+        overheated = set()
+
+        for system, check_results in system_check_results.items():
+            if any(check_result.online for check_result in check_results):
+                online.add(system)
+
+            if all(
+                    check_result.download < MIN_DOWNLOAD
+                    for check_result in check_results
+            ):
+                download_critical.add(system)
+
+            if all(
+                    check_result.upload < MIN_UPLOAD
+                    for check_result in check_results
+            ):
+                upload_critical.add(system)
+
+            if system.deployment is not None and not system.fitted:
+                not_fitted.add(system)
+
+            if all(
+                    check_result.sensors is SuccessFailedUnsupported.FAILED
+                    for check_result in check_results
+            ):
+                overheated.add(system)
+
+        return cls(
+            frozenset(system_check_results),
+            frozenset(online),
+            frozenset(download_critical),
+            frozenset(upload_critical),
+            frozenset(not_fitted),
+            frozenset(overheated)
+        )
