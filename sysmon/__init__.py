4b702b61b8adf83ac153c9edf654d0213c29a6a9
Richard Neumann <r.neumann@homeinfo.de>
2023-03-22 10:38:09 +0100

    Export send_mailing()

--- sysmon/__init__.py
+++ sysmon/__init__.py
@@ -1,7 +1,8 @@
 """HOMEINFO digital signage systems monitoring."""
 
+from sysmon.mailing import send_mailing
 from sysmon.orm import CheckResults, OfflineHistory
 from sysmon.wsgi import APPLICATION
 
 
-__all__ = ['APPLICATION', 'CheckResults', 'OfflineHistory']
+__all__ = ['APPLICATION', 'CheckResults', 'OfflineHistory', 'send_mailing']
