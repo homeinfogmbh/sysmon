866960191fa1de9be9ee60882906c3869a4411e4
Tobias Erlacher <40563361+aterriblechaos@users.noreply.github.com>
2023-09-12 09:14:16 +0200

    Newsletter access right decorators

--- sysmon/wsgi.py
+++ sysmon/wsgi.py
@@ -35,19 +35,23 @@ __all__ = ["APPLICATION"]
 APPLICATION = Application("sysmon")
 SERVICE_UNITS = {"hipster": "hipster.service", "sysmon": "sysmon.service"}
 
-
+@authenticated
+@authorized("sysmon")
+@root
 @APPLICATION.route("/patch_newsletter/<int:newsletter>", methods=["POST"], strict_slashes=False)
 def patch_newsletter(newsletter: int):
     nl = Newsletter.select().where(Newsletter.id == newsletter)
     nl.patch_json(request.json)
     nl.save()
 
-
+@authenticated
+@authorized("sysmon")
 @APPLICATION.route("/newsletter/<int:newsletter>", methods=["GET"], strict_slashes=False)
 def get_newsletter(newsletter: int):
     return list(Newsletter.select().where(Newsletter.id == newsletter).dicts())
 
-
+@authenticated
+@authorized("sysmon")
 @APPLICATION.route("/newsletters", methods=["GET"], strict_slashes=False)
 def get_newsletters():
     """List Newsletters."""
