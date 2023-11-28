f593265e5ca8e7d696b5cadbf009ff2210f56e7f
Tobias Erlacher <40563361+aterriblechaos@users.noreply.github.com>
2023-11-28 07:11:07 +0100

    Update orm.py

--- sysmon/orm.py
+++ sysmon/orm.py
@@ -74,7 +74,8 @@ class Newsletter(SysmonModel):
 
     def to_json(self, **kwargs) -> dict:
         json = super().to_json(**kwargs)
-
+        if json["image"]:
+            json["image"] = self.image.bytes
         return json
 
     def from_json(cls, json: dict, **kwargs) -> Transaction:
