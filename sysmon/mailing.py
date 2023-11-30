ab058547e12c64611c5691f39edf062215f4be50
Tobias Erlacher <40563361+aterriblechaos@users.noreply.github.com>
2023-11-30 07:16:13 +0100

    Update mailing.py

--- sysmon/mailing.py
+++ sysmon/mailing.py
@@ -202,7 +202,7 @@ a {{text-decoration: none;}}a[x-apple-data-detectors] {{ color: inherit !importa
   <tr>
      <td>
         <![endif]-->
-        <table class="t11of12 layout" style="border-collapse: collapse; max-width: 616px; width: 100%;" cellspacing="0" cellpadding="0" border="0" align="left">
+        <table class="t11of12 layout" style="border-collapse: collapse;  width: 100%;" cellspacing="0" cellpadding="0" border="0" align="left">
            <tbody><tr>
               <td style="direction:ltr;text-align:left;font-size: 1px; height: 1px; line-height: 1px; padding-left: 0px !important; padding-right: 0px !important; padding-top: 0px;">
                  <table style="border-collapse: collapse; table-layout: fixed; width: 100%;" width="100%" cellspacing="0" cellpadding="0" border="0" align="left">
@@ -226,11 +226,11 @@ a {{text-decoration: none;}}a[x-apple-data-detectors] {{ color: inherit !importa
   <td style="direction:ltr;text-align:left;">
 
 <!--[if (gte mso 9)|(IE)]>
-<table width="504" align="left" cellpadding="0" cellspacing="0" border="0">
+<table width="604" align="left" cellpadding="0" cellspacing="0" border="0">
   <tr>
      <td>
         <![endif]-->
-        <table class="t9of12 layout" style="border-collapse: collapse; max-width: 504px; width: 100%;" cellspacing="0" cellpadding="0" border="0" align="left">
+        <table class="t9of12 layout" style="border-collapse: collapse; width: 100%;" cellspacing="0" cellpadding="0" border="0" align="left">
            <tbody><tr>
               <td style="direction:ltr;text-align:left;font-size: 1px; height: 1px; line-height: 1px; padding-left: 0px !important; padding-right: 0px !important;">
                  <table style="border-collapse: collapse; table-layout: fixed; width: 100%;" width="100%" cellspacing="0" cellpadding="0" border="0" align="left">
