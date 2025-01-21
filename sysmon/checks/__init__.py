91dee38aaa1ad9e4432a829ff2f277310ab551e8
Tobias Erlacher <40563361+aterriblechaos@users.noreply.github.com>
2025-01-21 09:20:22 +0100

    limit check processes to 3
    Bandwith issue?

--- sysmon/checks/__init__.py
+++ sysmon/checks/__init__.py
@@ -38,7 +38,7 @@ TCP_TIMEOUT = 5  # seconds
 def check_systems(systems: Iterable[System], *, chunk_size: int = 10) -> None:
     """Checks the given systems."""
 
-    with Pool() as pool:
+    with Pool(processes=3) as pool:
         pool.map(check_system, systems, chunksize=chunk_size)
 
 
