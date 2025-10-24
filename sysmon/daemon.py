ffcc4cc1d4d8e3a19d82721a8fc5e722617eb694
Tobias Erlacher <40563361+aterriblechaos@users.noreply.github.com>
2025-10-24 06:14:17 +0200

    Dont check Systems without deployments

--- sysmon/daemon.py
+++ sysmon/daemon.py
@@ -20,7 +20,7 @@ def spawn() -> None:
     basicConfig(level=INFO, format=LOG_FORMAT)
     check_systems(
         System.select(cascade=True).where(
-            System.deployment != None & System.isvirtual == 0
+            System.deployment.is_null(False) & System.isvirtual == 0
         )
     )
     update_offline_systems(date.today(), blacklist=load_blacklist())
